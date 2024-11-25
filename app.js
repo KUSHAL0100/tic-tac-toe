363c35c192e857b842cc63e2eda4b73220d5cb20
unknown <kushalthakkar2024@gmail.com>
2024-11-25 17:18:44 +0530

    completed tic-tac-toe

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 let boxs=document.querySelectorAll(".box")
 let turn ='x';
 let isWin=false
-let ding=new Audio()
+let ding=new Audio("ding.mp3")
+let win=new Audio("win.mp3")
+let line=document.querySelector(".line")
 let changeTurn=()=>{
     turn=(turn==='x'? 0 : 'x')
 }
 let checkWin=()=>{
     let boxtexts=document.querySelectorAll(".boxtext")
     let arr=[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+        [0,1,2,0,5,0],
+        [3,4,5,0,15,0],
+        [6,7,8,0,25,0],
+        [0,3,6,-10,15,90],
+        [1,4,7,0,15,90],
+        [2,5,8,10,15,90],
+        [0,4,8,0,15,45],
+        [2,4,6,0,15,-45],
     ]
     arr.forEach((e )=>{ //ahi e etle [0,1,2] ee e che and e[1] ma 1 avse next itereation ma e aeee [3,4,5] kevase ema e[1] etle 4 ganase
         if((boxtexts[e[0]].innerText=== boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !==''))
         {
             isWin=true;
             document.querySelector(".info").innerText=turn+" Won";
-            document.querySelector(".winGif").style.width="20vw"
+            document.querySelector(".winGif").style.width="20vw";
+            boxtexts[e[0]].style.color="rgb(151 139 139)";
+            boxtexts[e[1]].style.color="rgb(151 139 139)";
+            boxtexts[e[2]].style.color="rgb(151 139 139)";
+            line.style.width="30vw"
+            line.style.transform=`translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`;
+            win.play();
         }
     })
 }
@@ -32,6 +40,8 @@ boxs.forEach(box =>{
         if(text.innerText==='' && !isWin )
         {
             text.innerText=turn;
+            ding.currentTime = 0;
+            ding.play();
             checkWin();
             if(!isWin){
                 changeTurn();
@@ -49,5 +59,7 @@ let reset=()=>{
         turn='x';
         document.querySelector(".info").innerText="Now "+turn+" turn"
         isWin=false;
+        boxT.style.color="black";
+        line.style.width="0"
     })
 }
